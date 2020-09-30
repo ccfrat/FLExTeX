7b2c68a3d529f6ba1dd2e41a93fe36e07b1d9835
ccfrat <ccfrattarelli@gmail.com>
2020-09-30 01:06:23 -0400

    improved error notifications
    added popups to display error details and offer solution tips for a common issue

--- src/ui/Start.java
+++ src/ui/Start.java
@@ -3,6 +3,8 @@ package ui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import main.*;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import javax.swing.*;
 import java.awt.*;
@@ -23,7 +25,18 @@ public class Start extends Application {
             new TagDictionary();
             testReader.setWriter(testWriter);
             testReader.processAll();
-        } catch (Exception e) {
+        } catch (SAXParseException e) {
+            JOptionPane.showMessageDialog(null,
+                    "An error occurred while parsing the file. \n" +
+                            "FLEx sometimes exports an XML file with \n" +
+                            "markup declarations that give this program \n" +
+                            "trouble. Try deleting the stuff in the second \n" +
+                            "set of brackets, from \"<!DOCTYPE\" \n all \n" +
+                            "the way through the following \">\" and try again.");
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null,e.getMessage(),
+                    "An Error Occurred", JOptionPane.INFORMATION_MESSAGE);
             e.printStackTrace();
         }
     }
